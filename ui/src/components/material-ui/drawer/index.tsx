d36f37ed8f753d34f3525b07a29603fdceed643a
Kevin Shirley <kevinxshirley@gmail.com>
2019-12-31 10:27:43 -0500

    Next link module bug

--- ui/src/components/material-ui/drawer/index.tsx
+++ ui/src/components/material-ui/drawer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/Link';
+import Link from 'next/link';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
