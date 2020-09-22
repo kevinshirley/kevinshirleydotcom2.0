bf21ffba3fceffc152b2490efe91cdd993e8d578
Kevin Shirley <kevinxshirley@gmail.com>
2020-09-22 01:28:30 -0400

    Added items to portfolio

--- src/components/about/about-section-1.tsx
+++ src/components/about/about-section-1.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 function AboutSection1() {
   return (
     <div className="about-section-1">
+      <span>The Story</span>
       <h2>About me</h2>
     </div>
   );
--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -1,14 +1,23 @@
-import bgImg from 'src/assets/images/do-something-great.jpg';
 import tictactoe from 'src/assets/images/tictactoe.png';
 import tripimagine from 'src/assets/images/tripimagine.png';
 import taquin from 'src/assets/images/taquin.png';
 import pokergame from 'src/assets/images/pokergame.png';
 import shoptripimagine from 'src/assets/images/shoptripimagine.png';
 import addressbook from 'src/assets/images/addressbook.png';
+import travelDashboard from 'src/assets/images/travel-dashboard.png';
+import autoturbobuying from 'src/assets/images/autoturbobuying.png';
 
 export const PORTFOLIO = [
   {
     index: 0,
+    title: 'Travel Dashboard',
+    description: 'Web application to manage travel itineraries',
+    image: travelDashboard,
+    projectUrl: 'https://travel-dashboard-landing-page.vercel.app/en',
+    codeUrl: 'https://github.com/kevinshirley/travel-dashboard',
+  },
+  {
+    index: 1,
     title: 'Tic Tac Toe Game',
     description: 'Made with React',
     image: tictactoe,
@@ -16,7 +25,7 @@ export const PORTFOLIO = [
     codeUrl: 'https://github.com/kevinshirley/tictactoe',
   },
   {
-    index: 1,
+    index: 2,
     title: 'Trip Imagine | Travel Agency',
     description: 'Made with React',
     image: tripimagine,
@@ -24,15 +33,15 @@ export const PORTFOLIO = [
     codeUrl: 'https://github.com/kevinshirley/tripimagine2.0',
   },
   {
-    index: 2,
+    index: 3,
     title: 'Taquin Puzzle Game',
     description: 'Made with React',
     image: taquin,
-    projectUrl: 'http://kevinshirley.com/index.php?action=taquin',
+    projectUrl: 'http://taquin.kevinshirley.com/',
     codeUrl: 'https://github.com/kevinshirley/taquinPuzzle',
   },
   {
-    index: 3,
+    index: 4,
     title: 'Poker Cards Game',
     description: 'Made with Angular',
     image: pokergame,
@@ -40,7 +49,7 @@ export const PORTFOLIO = [
     codeUrl: 'https://www.github.com/kevinshirley',
   },
   {
-    index: 4,
+    index: 5,
     title: 'Trip Imagine Shop',
     description: 'Made with PHP',
     image: shoptripimagine,
@@ -48,11 +57,19 @@ export const PORTFOLIO = [
     codeUrl: 'https://www.github.com/kevinshirley',
   },
   {
-    index: 5,
+    index: 6,
     title: 'Address Book',
     description: 'Made with Angular',
     image: addressbook,
     projectUrl: 'http://addressbook.kevinshirley.com/',
     codeUrl: 'https://github.com/kevinshirley/addressbook',
+  },
+  {
+    index: 7,
+    title: 'Auto Turbo',
+    description: 'Made with React',
+    image: autoturbobuying,
+    projectUrl: 'https://auto-turbo-buying.vercel.app/en',
+    codeUrl: 'https://github.com/kevinshirley/auto-turbo-buying',
   },
 ];
