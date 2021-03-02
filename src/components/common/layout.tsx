cfee827815e8f65507712f64fbcc97ab053ac828
Kevin Shirley <kevinxshirley@gmail.com>
2021-03-02 08:07:41 -0500

    Meta data

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import laptopDesk from 'src/assets/images/laptop-desk.jpg';
 
 import 'src/styles/app.scss';
 
@@ -8,6 +9,10 @@ function Layout(props: any) {
     <div className="container layout">
       <Head>
         <title>Kevin Shirley | Full Stack Software Developer</title>
+        <meta name="description" content="I build modern day apps, ecommerce platforms, web applications and more. Technology and strategy." />
+        <meta property="og:title" content="Kevin Shirley | Full Stack Software Developer" key="title" />
+        <meta property="og:description" content="I build modern day apps, ecommerce platforms, web applications and more. Technology and strategy." />
+        <meta property="og:image" content={laptopDesk} />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <script src="https://code.iconify.design/1/1.0.3/iconify.min.js"></script>
